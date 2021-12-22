0ae73f9d99a43232592d497a5f15f41f647ec6b4
Yohan <muliono.yohan@gmail.com>
2021-12-22 13:09:52 +0700

    nambahin fungsi pengurangan

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def penambahan(a,b):
 	total = a+b
 	return total
 
+def pengurangan(a,b):
+	total = a-b
+	return total
+
 def main():
 	print (penambahan(5,5))
+	print (pengurangan(10,5))
 
 main()
 
